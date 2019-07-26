181c509d4ea7f9a3a28dc36920cc782867c9c81b
noobying <167479442@qq.com>
2019-07-26 18:59:50 +0800

    hoare的快速排序

--- code/第七章 快速排序/第七章 快速排序/源.cpp
+++ code/第七章 快速排序/第七章 快速排序/源.cpp
@@ -2,8 +2,7 @@
 #include <ctime>
 using namespace std;
 
-int a[1000000];
-int b[1000000];
+
 void random_array(int *a, int n, int range)
 {
 	srand((unsigned)time(NULL));
@@ -41,6 +40,44 @@ int partition(int *a, int left, int right)
 	else
 		return i + 1;
 }
+int partition_hoare(int *a, int left, int right)
+{
+	/*Hoare划分*/
+	int x = a[left];
+	int l = left-1 ;
+	int r = right+1;
+	while (true)
+	{
+		do l++; while (a[l] < x);
+		do r--; while (a[r] > x);
+		//for (; x > a[l]&&l<=right; l++);
+		//for (; x < a[r]&&r>=left; r--);
+		if (r > l)
+		{
+			int temp = a[l];
+			a[l] = a[r];
+			a[r] = temp;
+		}
+		else
+			return r;
+	}
+}
+int hoare_partition(int A[], int p, int r) {
+	int x = A[p],
+		i = p - 1,
+		j = r+1,
+		tmp;
+	while (true) {
+		do { j--; } while (!(A[j] <= x));
+		do { i++; } while (!(A[i] >= x));
+		if (i < j) {
+			tmp = A[i]; A[i] = A[j]; A[j] = tmp;
+		}
+		else {
+			return j;
+		}
+	}
+}
 void quicksort(int *a, int left, int right)
 {
 	if (left < right)
@@ -50,6 +87,16 @@ void quicksort(int *a, int left, int right)
 		quicksort(a, left, mid - 1);
 		quicksort(a, mid + 1, right);
 	}
+}
+void quicksort_hoare(int *a, int left, int right)
+{
+	if (left < right)
+	{
+		int mid =partition_hoare(a, left, right);
+		quicksort_hoare(a, left, mid);
+		quicksort_hoare(a, mid + 1, right);
+	}
+
 }
 int partition_random(int *a, int left, int right)
 {
@@ -114,9 +161,11 @@ void print_array(int *a, int n)
 int main()
 {
 	clock_t start, end;
-	int n = 400000;  
-	random_array(a, n, 10000000);
-	for (int i = 0; i < n; i++)
+	int n = 4;  
+	int a[4] = {4,2,1,3};
+	/*random_array(a, n, 100);*/
+
+	/*for (int i = 0; i < n; i++)
 		b[i] = a[i];
 	start = clock();
 	quicksort(a, 0, n - 1);
@@ -125,11 +174,11 @@ int main()
 	start = clock();
 	quicksort_limit(a, 0, n - 1, 550);
 	end = clock();
-	cout << "限制了:" << endl << double(end - start) / (CLOCKS_PER_SEC) << endl;
+	cout << "限制了:" << endl << double(end - start) / (CLOCKS_PER_SEC) << endl;*/
+	print_array(a, n);
+	quicksort_hoare(a, 0, n - 1); 
+	print_array(a, n);
+	/*cout << hoare_partition(a, 0, n - 1);*/
 	system("pause");
-
-
-
-
 	return 0;
 }
