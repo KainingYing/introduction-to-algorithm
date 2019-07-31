b31f2bfa61af6ea03c89abeddd70f6a615d569db
noobying <167479442@qq.com>
2019-07-31 09:11:52 +0800

    钢条切割 完善版

--- code/第十五章 动态规划/第十五章 动态规划/钢条切割.cpp
+++ code/第十五章 动态规划/第十五章 动态规划/钢条切割.cpp
@@ -27,8 +27,6 @@ int memoized_cut_rod(int *p, int n,int *r)
 	}
 	return memoized_cut_rod_aux(p, n, r);
 }
-
-
 int bottom_up_cut_rod(int *p,int n,int *r)
 {
 	if (n == 0)
@@ -45,7 +43,6 @@ int bottom_up_cut_rod(int *p,int n,int *r)
 	}
 	return r[n];
 }
-
 int extended_bottom_up_rod(int *p, int n,int *r,int *s)
 {
 	r[0] = 0;
@@ -99,12 +96,50 @@ void print_cut_rod_solution(int *p, int n,int *r,int *s)
 		n = n - s[n];
 	}
 }
+int extended_memoized_cut_rod_aux(int *p, int n, int *r, int *s)
+{
+	if (r[n] >= 0)
+		return r[n];
+	int q;
+	if (n == 0)
+		q = 0;
+	else
+	{
+		q = -1;
+		for (int i = 0; i <= n; i++)
+		{
+			int value = extended_memoized_cut_rod_aux(p, n - i, r, s);
+			if (q < p[i] + value)
+			{
+				q = value;
+				s[n] = i;
+				r[n] = q;
+			}
+		}
+	}
+	return r[n];
+}
+int extended_memoized_cut_rod(int *p, int n, int *r, int *s)
+{
+	for (int i = 0; i <= n; i++)
+		r[i] = -1;
+	return extended_memoized_cut_rod_aux(r, n, r, s);
+}
+void print_momorized_cut_rod_solution(int *p, int n, int *r, int *s)
+{
+	extended_bottom_up_rod(p,n,r,s);
+	while (n > 0)
+	{
+		cout << s[n] << ' ';
+		n = n - s[n];
+	}
+}
 int main()
 {
 	int p[11] = { 0,1,5,8,9,10,17,17,20,24,30 };
 	int *r = new int[100];
 	int *s = new int[100];
-	print_cut_rod_solution(p, 10, r, s);
+	print_momorized_cut_rod_solution(p, 7, r, s);
 
 
 	system("pause");
