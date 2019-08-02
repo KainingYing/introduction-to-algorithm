b073ddf64c09f72d3969f3d60bcbe0afe80e8c21
noobying <167479442@qq.com>
2019-08-02 16:00:14 +0800

    自底向上加构造最优解

--- code/第十五章 动态规划/第十五章 动态规划/LCScpp.cpp
+++ code/第十五章 动态规划/第十五章 动态规划/LCScpp.cpp
@@ -17,12 +17,65 @@ int LCS_crude(int a, int b,int *x,int *y)
 		return (c > d ? c : d);
 	}
 }
+void LCS_length(int m, int n, int *x, int *y,int **c,int **b)
+{
+	//这是一个自顶向上的方式 m与n为数组的长度
+	for (int i = 0; i <= m; i++)//先进行初始化
+		c[i][0] = 0;
+	for (int i = 0; i <= n; i++)
+		c[0][i] = 0;
+	for(int i=1;i<=m;i++)
+		for (int j = 1; j <= n; j++)
+		{
+			if (x[i - 1] == y[j - 1])
+			{
+				c[i][j] = c[i - 1][j - 1] + 1;
+				b[i][j] = 1;
+			}
+			else if (c[i - 1][j] >= c[i][j - 1])
+			{
+				c[i][j] = c[i-1][j];
+				b[i][j] = 2;
+			}
+			else
+			{
+				c[i][j] = c[i][j-1];
+				b[i][j] = 3;
+			}
+		}
+}
+void print_LCS(int **b,int *x,int i,int j)
+{
+	if (i == 0 || j == 0)
+		return;
+	if (b[i][j] == 1)
+	{
+		print_LCS(b, x, i - 1, j - 1);
+		cout << x[i - 1] << " ";
+	}
+	else if (b[i][j] == 2)
+	{
+		print_LCS(b, x, i - 1, j);
+	}
+	else
+		print_LCS(b, x, i, j - 1);
+}
 int main()
 {
 	int x[3] = { 1,1,5 };
 	int y[3] = {1,1,4};
-	cout << LCS_crude(2, 2, x, y) << endl;
-	system("pause");
+	int **b = new int*[10];
+	for (int i = 0; i < 10; i++)
+		b[i] = new int[10];
+	int **c = new int*[10];
+	for (int i = 0; i < 10; i++)
+		c[i] = new int[10];
 
+	LCS_length(3, 3, x, y, c, b);
+	print_LCS(b, x, 3, 3);
+	delete[]b;
+	delete[]c;
+
+	system("pause");
 	return 0;
 }
