e88289b63a1edd09b111371314939b51fa781717
noobying <167479442@qq.com>
2019-08-03 11:35:10 +0800

    备忘录

--- code/第十五章 动态规划/第十五章 动态规划/LCScpp.cpp
+++ code/第十五章 动态规划/第十五章 动态规划/LCScpp.cpp
@@ -19,7 +19,7 @@ int LCS_crude(int a, int b,int *x,int *y)
 }
 void LCS_length(int m, int n, int *x, int *y,int **c,int **b)
 {
-	//这是一个自顶向上的方式 m与n为数组的长度
+	//这是一个自顶、底向上的方式 m与n为数组的长度
 	for (int i = 0; i <= m; i++)//先进行初始化
 		c[i][0] = 0;
 	for (int i = 0; i <= n; i++)
@@ -76,6 +76,40 @@ void print_LCS_version2(int **c, int *x, int i, int j)
 	else
 		print_LCS_version2(c, x, i, j - 1);
 }
+int LCS_length_up2down_aux(int m, int n, int **c, int *x, int *y)
+{
+	if (c[m][n] != -1)
+		return c[m][n];
+	else
+	{
+		if (m == 0 || n == 0)
+		{
+			c[m][n] = 0;
+			return c[m][n];
+		}
+		else if (x[m-1] == y[n-1])
+		{
+			c[m][n] = LCS_length_up2down_aux(m - 1, n - 1, c, x, y)+1;
+			return c[m][n];
+		}
+		else
+		{
+			int a = LCS_length_up2down_aux(m, n - 1, c, x, y);
+			int b = LCS_length_up2down_aux(m - 1, n, c, x, y);
+			c[m][n] = a > b ? a : b;
+			return c[m][n];
+		}
+	}
+
+}
+void LCS_length_up2down(int m, int n, int **c, int *x, int *y)
+{
+	for (int i = 0; i <= m; i++)
+		for (int j = 0; j <= n; j++)
+			c[i][j] = -1;
+	LCS_length_up2down_aux(m, n, c, x, y);
+	
+}
 int main()
 {
 	int x[8] = { 1,0,0,1,0,1,0,1 };
@@ -87,10 +121,10 @@ int main()
 	for (int i = 0; i < 10; i++)
 		c[i] = new int[10];
 
-	LCS_length(8, 9, x, y, c, b);
-	print_LCS(b, x, 3, 3);
+	LCS_length_up2down(8, 9, c, x, y);
+	//print_LCS(b, x, 3, 3);
 	cout << endl;
-	print_LCS_version2(c, x, 3, 3);
+	//print_LCS_version2(c, x, 3,3);
 	delete[]b;
 	delete[]c;
 
